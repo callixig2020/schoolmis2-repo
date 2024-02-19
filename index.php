92ef2acea80aa091a710db8f24ece7fbab2126b9
callixig <callixig@gmail.com>
2024-02-19 14:36:40 +0200

    well commited file

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo"<h2> welcome to my docker page today!!!!!!!!<h2>";
+    ?>
+</body>
+</html>
